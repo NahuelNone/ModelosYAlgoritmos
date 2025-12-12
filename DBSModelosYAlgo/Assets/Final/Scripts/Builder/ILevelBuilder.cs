aa1cb749d6e4cf0c9053434d2c96687d144bb7d5
NahuelCarro27 <nahuel.carro@davinci.edu.ar>
2025-12-11 21:30:10 -0300

    builder borra3

--- DBSModelosYAlgo/Assets/Final/Scripts/Builder/ILevelBuilder.cs
+++
@@ -1,9 +0,0 @@
-public interface ILevelBuilder
-{
-    void Reset();              // Limpiar nivel anterior
-    void BuildGround();        // Suelo/base
-    void BuildPlatforms();     // Plataformas
-    void BuildEnemies();       // Enemigos
-    //void BuildCollectibles();  // Ítems/monedas
-    Level GetResult();         // Devolver el Level construido
-}
